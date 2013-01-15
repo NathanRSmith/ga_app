840ef9991243293b94f5cc8949927dcc0a1261bc
Nathan Smith <nsmith13@nd.edu>
2013-01-15 16:56:39 -0500

    commented out some lines to make it work with tastypie trunk

--- ga_app/admin.py
+++ ga_app/admin.py
@@ -4,8 +4,8 @@ from tastypie.models import ApiAccess, ApiKey
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-admin.site.register(ApiKey)
-admin.site.register(ApiAccess)
+#admin.site.register(ApiKey)
+#admin.site.register(ApiAccess)
 
 class UserModelAdmin(UserAdmin):
     inlines = UserAdmin.inlines + [ApiKeyInline]
